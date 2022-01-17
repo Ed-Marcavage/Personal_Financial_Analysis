c0b8513dbd394925886536ceb8f21f615b93ceb2
edmarcavage11 <54510433+edmarcavage11@users.noreply.github.com>
2022-01-17 17:05:36 -0500

    added basic logic to store user input into a CSV file

--- func.py
+++ func.py
@@ -1,3 +1,9 @@
+import csv
+from dataclasses import field
+import shutil
+from tempfile import NamedTemporaryFile
+
+
 class InputNotInRangeError(Exception):
     """Exception raised for numbers out of range
 
@@ -15,4 +21,86 @@ class InputNotInRangeError(Exception):
 def InputNotInRange(input, lower, upper):
   if not lower <= input <= upper:
     raise InputNotInRangeError(input)
+
+
+def PrintDictValues(dict):
+  i = 0 
+  for keys in dict:
+    print(list(dict.keys())[i], list(dict.values())[i])
+    i += 1
+  print()
+
+
+def DoubleCheck(Dict, RangeMin, RangeMax):
+  while True:
+    category = int(input("Enter the number that corresponds to the income category: "))
+    print()
+    InputNotInRange(category, RangeMin, RangeMax)
+    In_DubChck = input("Is {} correct? (Y/N): ".format(Dict.get(category)))
+    if In_DubChck.upper() == "Y":
+      return Dict.get(category)
+      break
+    elif In_DubChck.upper() == "N":
+      print()
+    else:
+      print("Invalid input...Please choose again...\n")
+
+# GET LENGTH OF CSV FILE FOR INDEXING
+def get_length(file_path):
+    with open(file_path) as f:
+        reader = csv.reader(f)
+        reader_list = list(reader)
+        return len(reader_list)
+
+# CREATE AND THEN APPEND DATA TO THE END OF A CSV FILE
+def append_data(file_path, date, Transaction_Type, Description, Amount):
+    fieldNames = ['id','Date', 'Transaction Type', 'Description', 'Amount']
+    next_id = get_length(file_path)
+
+    #Creates Transactions.csv if it is not created yet (first time running code)
+    with open(file_path, 'a') as f:
+        writer = csv.DictWriter(f, fieldnames=fieldNames)
+
+        if f.tell() ==0:
+          writer.writeheader() #-- only run this line once to add CSV headings only once
+        writer.writerow({
+            'id': next_id,
+            'Date': date,
+            'Transaction Type': Transaction_Type,
+            'Description': Description,
+            'Amount': Amount
+        })
+
+#append_data('Transactions.csv', '10-2-2010', 'income', 'salary', '1000')
+
+
+# EDIT CSV FILE ROW BASED ON INDEX
+def edit_data(file_path, edit_id, Edit_Transaction_Type, Edit_Description, Edit_Amount):
+    temp_f = NamedTemporaryFile(mode="w",newline= '', delete=False)
+
+    with open(file_path, "r") as f, temp_f:
+        reader =  csv.DictReader(f)
+        fieldnames = ['id','Date', 'Transaction Type', 'Description', 'Amount']
+        writer = csv.DictWriter(temp_f, fieldnames = fieldnames)
+        writer.writeheader()
+        for row in reader:
+            if int(row['id']) == int(edit_id):
+                row['Transaction Type'] = Edit_Transaction_Type
+                row['Description'] =  Edit_Description
+                row['Amount'] = Edit_Amount
+
+            writer.writerow(row)
+
+    shutil.move(temp_f.name, file_path)
+    return True 
+
+    return False
+
+
+#edit_data('Transactions.csv', edit_id=1, Edit_Transaction_Type = "expense", Edit_Description = "health", Edit_Amount=1000)
+
+
+
+
+
 
--- personal_fin_main.py
+++ personal_fin_main.py
@@ -1,17 +1,27 @@
+#from tokenize import Double
 import func
+from datetime import date
+from pathlib import Path
+
+
+myfile = Path('Transactions.csv')
+myfile.touch(exist_ok=True)
+#f = open(myfile)
 
 # Functions needed in this module 
 
 Break = 1
-def EndLoopFunc():
+def EndLoopFunc(IncomeOrExpense, category, amount):
   global Break #Break var4 is out of func scope w/o Global keyword
   while True: 
         try:
           Cont = int(input("Enter 1 to conduct another transaction, enter 2 to quit: "))
           if Cont == 1:
+            func.append_data('Transactions.csv', date.today(), IncomeOrExpense, category, amount)
             break
           else:
             print("saving input to tranasctions.csv file...\n")
+            func.append_data('Transactions.csv', date.today(), IncomeOrExpense, category, amount)
             Break = 0
             break
         except (ValueError, func.InputNotInRangeError):
@@ -35,25 +45,35 @@ while Break:
     # Exception handling ensuring numeric value (float)
     while True:
       try:
-        Expenses = float(input("Enter income amount in USD: "))
+        Income = float(input("Enter income amount in USD: "))
+
         break
       except ValueError:
         print("Please only enter numeric values\n")
 
     # Exception handling ensuring numeric value and between 1-4
-    print("1. Salary\n2. Interest\n3. Tax Return\n4. Side Hustle\n")
+    #print("1. Salary\n2. Interest\n3. Tax Return\n4. Side Hustle\n")
+    
+    Income_Dic = {1: "Salary", 
+                  2: "Interest", 
+                  3: "Tax Return", 
+                  4: "Side Hustle"}
+
+    func.PrintDictValues(Income_Dic)
+
+  
     while True: 
+
       try:
-        category = int(input("Enter the number that corresponds to the income category: "))
-        func.InputNotInRange(category, 1, 4)
-        # if not 0 < category < 5:
-        #   raise InputNotInRangeError(Response)
+        category = func.DoubleCheck(Income_Dic, 1, 4)
         break
+
       except (ValueError, func.InputNotInRangeError):
         print("Please only enter an interger from 1-4\n")
 
-    # Runs func to get user input; either breaks or cont main while loop 
-    EndLoopFunc()
+    # Runs func to get user input; either breaks or cont main while loop
+
+    EndLoopFunc("Income", category, Income)
 
 
   # IF INPUT IS AN EXPENSE
@@ -68,13 +88,31 @@ while Break:
 
 
     # Exception handling ensuring numeric value and between 1-10
-    print("1. Housing\n2. Transportation\n3. Food\n4. Utilities\n5. Insurance\n6. Healthcare\n7. Investing\n8. Personal\n9. Recreation\n10.Savings\n")
-    while True:
+    #print("1. Housing\n2. Transportation\n3. Food\n4. Utilities\n5. Insurance\n6. Healthcare\n7. Investing\n8. Personal\n9. Recreation\n10.Savings\n")
+
+    Expense_Dic = {1:"Housing",
+                   2:"Transportation", 
+                   3:"Food", 
+                   4:"Utilities", 
+                   5:"Insurance", 
+                   6:"Healthcare", 
+                   7:"Investing", 
+                   8:"Personal", 
+                   9:"Recreation", 
+                   10:"Savings"}
+
+    func.PrintDictValues(Expense_Dic)
+
+    while True: 
       try:
-        category = int(input("Enter the number that corresponds to the expense category: "))
-        func.InputNotInRange(category, 1, 10)
+        ExpenseCategory = func.DoubleCheck(Expense_Dic, 1, 10)
+      #category = int(input("Enter the number that corresponds to the expense category: "))
+      #func.InputNotInRange(category, 1, 10)
         break
       except (ValueError, func.InputNotInRangeError):
-        print("Please only enter an interger from 1-10\n")
+       print("Please only enter an interger from 1-10\n")
 
-    EndLoopFunc()
+
+    EndLoopFunc("Expense", ExpenseCategory, Expenses)
+
+
